b9e711b7c583e2c45101f578f7a87df601eeda84
NataliaTepluhina <tarya.se@gmail.com>
2018-08-14 11:12:01 +0300

    * configured webpack to allow graphql mocking

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,25 @@
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
+      extensions: ['*', '.mjs', '.js', '.vue', '.json', '.gql', '.graphql'],
+    },
+    module: {
+      rules: [
+        // fixes https://github.com/graphql/graphql-js/issues/1272
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto',
+        },
+      ],
+    },
+  },
   pluginOptions: {
     apollo: {
       enableMocks: true,
       enableEngine: true,
-      integratedEngine: false
-    }
-  }
-}
+      integratedEngine: false,
+    },
+  },
+};

975971b28356a2d79846fff1198432961d225089
NataliaTepluhina <tarya.se@gmail.com>
2018-10-27 22:10:48 +0300

    * changed apolloprovider

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import App from './App.vue'
-import {createProvider} from './vue-apollo'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import { createProvider } from './vue-apollo';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 new Vue({
-    provide: createProvider().provide(),
-    render: h => h(App)
+  apolloProvider: createProvider(),
+  render: h => h(App),
 }).$mount('#app');

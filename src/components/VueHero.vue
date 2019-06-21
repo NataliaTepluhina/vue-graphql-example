487cf1130dc502a16aeef6c22a6cb8bd4fdc8e52
Natalia Tepluhina <tarya.se@gmail.com>
2019-06-21 09:01:48 +0300

    Fixed cards height

--- src/components/VueHero.vue
+++ src/components/VueHero.vue
@@ -1,29 +1,36 @@
 <template>
-<v-flex md3 xs12>
-  <v-card height="100%">
-    <v-img height="250px"
-                  :src="hero.image"></v-img>
-    <v-card-title primary-title class="hero-title">
-      <div>
-        <h3 class="title">{{hero.name}}</h3>
-        <div class="hero-icons">
-          <a :href="`https://github.com/${hero.github}`" target="_blank" v-if="hero.github">
+  <v-flex md3 xs12>
+    <v-card>
+      <v-img height="250px" :src="hero.image"></v-img>
+      <v-card-title primary-title class="hero-title">
+        <div>
+          <h3 class="title">{{ hero.name }}</h3>
+          <div class="hero-icons">
+            <a
+              :href="`https://github.com/${hero.github}`"
+              target="_blank"
+              v-if="hero.github"
+            >
               <i class="fab fa-github"></i>
-          </a>
-          <a :href="`https://twitter.com/${hero.twitter}`" target="_blank" v-if="hero.github">
+            </a>
+            <a
+              :href="`https://twitter.com/${hero.twitter}`"
+              target="_blank"
+              v-if="hero.github"
+            >
               <i class="fab fa-twitter"></i>
-          </a>
+            </a>
+          </div>
         </div>
-      </div>
-    </v-card-title>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="$emit('deleteHero', hero.name)">
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="$emit('deleteHero', hero.name)">
           <v-icon>delete</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</v-flex>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
@@ -35,4 +42,3 @@ export default {
   },
 };
 </script>
-
